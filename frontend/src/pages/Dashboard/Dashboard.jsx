100746a3258988965bf208be362af659ac285925
Diego Lopez-Araiza Soto <140852928+BlackaxolotlORG@users.noreply.github.com>
2024-04-19 18:12:14 -0600

    Cambios a home  page y header

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,20 @@
-
+// ******************** Styles ********************
+import styles from './Dashboard.module.css';
+// **************** Images ****************
+import lunaAxImage from '../../assets/img/TiendaM.png';
 
 const Dashboard = () => {
     return (
         <div>
-            <h1>Dashboard</h1>
-            <div>
-                <h4>Hola Diego</h4>
-                <p>Tienes <span style={{ color: 'blue' }}>4 notificaciones</span> </p>
+            <div className={styles["Dashboard"]}>
+                <div className={styles["Profile"]}>
+                <div>
+                               <img className={styles["ProfilePic"]} src={lunaAxImage} alt="Axolotl-Waiting" />
+                               </div>
+                <h2 className={styles["ProfileHead"]}>Hola Diego,</h2>
+                <p className={styles["ProfileHead"]}>Tienes <span style={{ color: 'blue' }}>4 notificaciones</span> </p>
+                <p className={styles["ProfileHead"]}>Sparkaxe</p>
+                </div>
             </div>
         </div>
     )
