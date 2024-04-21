372c365eca7f0419268d61d6425f2a66685a775b
Diego Lopez-Araiza Soto <140852928+BlackaxolotlORG@users.noreply.github.com>
2024-04-21 16:01:03 -0600

    landing page start

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,22 @@
+// ******************** Styles ********************
+import styles from '../styles/Home.module.css';
+
 import { Link } from "react-router-dom"
 
 
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
+            <div className={styles.nav}>
+            <ul className={styles.navbar}>
+<li className={styles.navB}><a href="#home">Home</a></li>
+  <li className={styles.navB}><a href="#news">News</a></li>
+  <li className={styles.navB}><a href="#contact">Contact</a></li>
+  <Link to="/signup"><li className={styles.rightnavB}><a className="active" href="#about">Sign up</a></li></Link>
+  <Link to="/login"><li className={styles.rightnavB}><a className="active" href="#about">Login</a></li></Link>
+</ul>
+            </div>
+            <h1 className={styles.home}>Home</h1>
         </div>
     )
 }
