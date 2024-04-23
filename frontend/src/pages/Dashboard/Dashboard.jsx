adbaf0cef4d043a08b37b9ee4d741c01e7719091
Diego Lopez-Araiza Soto <140852928+BlackaxolotlORG@users.noreply.github.com>
2024-04-23 16:28:27 -0600

    hello

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -7,6 +7,7 @@ const Dashboard = () => {
     return (
         <div>
             <div className={styles["Dashboard"]}>
+            <div  className={styles["row"]}>
                 <div className={styles["Profile"]}>
                 <div>
                                <img className={styles["ProfilePic"]} src={lunaAxImage} alt="Axolotl-Waiting" />
@@ -19,6 +20,61 @@ const Dashboard = () => {
                     <div className={styles["Menu-item"]}>
                     Hello
                     </div>
+                    
+                </div>
+                <div className={styles["big"]}>
+                    <div className={styles["Menu-item"]}>
+                    Hello
+                    </div>
+                    </div>  
+                </div>
+
+
+
+
+                <div  className={styles["row"]}>
+                <div className={styles["big"]}>
+                    <div className={styles["Menu-item"]}>
+                    Hello
+                    </div>
+                    </div> 
+                <div className={styles["Profile"]}>
+                <div>
+                               <img className={styles["ProfilePic"]} src={lunaAxImage} alt="Axolotl-Waiting" />
+                               </div>
+                <h2 className={styles["ProfileHead"]}>Hola Diego,</h2>
+                <p className={styles["ProfileHead"]}>Tienes <span style={{ color: 'blue' }}>4 notificaciones</span> </p>
+                <p className={styles["ProfileHead"]}>Sparkaxe</p>
+                </div>
+                <div className={styles["Menu"]}>
+                    <div className={styles["Menu-item"]}>
+                    Hello
+                    </div>
+                    
+                </div>
+                 
+                </div>
+
+
+
+
+
+
+
+
+                <div  className={styles["row"]}>
+                <div className={styles["big"]}>
+                    <div className={styles["Menu-item"]}>
+                    Hello
+                    </div>
+                    </div> 
+                    <div className={styles["big"]}>
+                    <div className={styles["Menu-item"]}>
+                    Hello
+                    </div>
+                    </div> 
+                
+                 
                 </div>
             </div>
         </div>
