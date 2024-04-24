43bc532b8c12e20d81c326eca48cb16416595ae3
Diego Lopez-Araiza Soto <140852928+BlackaxolotlORG@users.noreply.github.com>
2024-04-23 19:28:57 -0600

    cambios home

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -2,11 +2,13 @@
 import styles from './Dashboard.module.css';
 // **************** Images ****************
 import lunaAxImage from '../../assets/img/TiendaM.png';
+import Vape from '../../assets/img/vape.webp';
 
 const Dashboard = () => {
     return (
         <div>
             <div className={styles["Dashboard"]}>
+                <h1>La mejor forma de manejar tu negocio a tu manera</h1>
             <div  className={styles["row"]}>
                 <div className={styles["Profile"]}>
                 <div>
@@ -16,17 +18,16 @@ const Dashboard = () => {
                 <p className={styles["ProfileHead"]}>Tienes <span style={{ color: 'blue' }}>4 notificaciones</span> </p>
                 <p className={styles["ProfileHead"]}>Sparkaxe</p>
                 </div>
-                <div className={styles["Menu"]}>
-                    <div className={styles["Menu-item"]}>
-                    Hello
-                    </div>
-                    
+                <div className={styles["Most"]}>
+                <img className={styles["Mostproduct"]} src={Vape} alt="Most sold product" />
+                    <h4>Producto mas vendido</h4>
                 </div>
                 <div className={styles["big"]}>
-                    <div className={styles["Menu-item"]}>
-                    Hello
+                    <div className={styles["bigtop"]}>
+                    <p><i className="fa-solid fa-dolly"></i>  Inventario bajo</p>
                     </div>
-                    </div>  
+                    <p>hello qorld</p>
+                    </div>     
                 </div>
 
 
@@ -44,7 +45,7 @@ const Dashboard = () => {
                                </div>
                 <h2 className={styles["ProfileHead"]}>Hola Diego,</h2>
                 <p className={styles["ProfileHead"]}>Tienes <span style={{ color: 'blue' }}>4 notificaciones</span> </p>
-                <p className={styles["ProfileHead"]}>Sparkaxe</p>
+                <p className={styles["ProfileHead"]}>Sparkaxe+</p>
                 </div>
                 <div className={styles["Menu"]}>
                     <div className={styles["Menu-item"]}>
