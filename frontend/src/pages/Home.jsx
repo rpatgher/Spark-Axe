e37853564e7eb978e9447ac040e495a17df4104a
Diego Lopez-Araiza Soto <140852928+BlackaxolotlORG@users.noreply.github.com>
2024-04-27 11:15:17 -0600

    cambios 1.0

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
             <Link to="/"><li className={styles.navB}><a>Inicio</a></li></Link>
             <Link to="/about"><li className={styles.navB}><a>Sobre Nosotros</a></li></Link>
             <Link to="/contact"><li className={styles.navB}><a>Contactanos</a></li></Link>
-            <Link to="/signup"><li className={styles.rightnavB}><a>Sign up</a></li></Link>
-            <Link to="/login"><li className={styles.rightnavB}><a>Login</a></li></Link>
+            <Link to="/signup"><li className={styles.rightnavB}><a>Registrarse</a></li></Link>
+            <Link to="/login"><li className={styles.rightnavB}><a>Iniciar Sesion</a></li></Link>
 </ul>
             </div>
             <h1 className={styles.home}>Home</h1>
