79065b3d34b0b43848c0c204f4896242ef19cdde
Diego Lopez-Araiza Soto <140852928+BlackaxolotlORG@users.noreply.github.com>
2024-05-10 12:47:31 -0600

    loading screen

--- frontend/src/layouts/ProtectedLayout.jsx
+++ frontend/src/layouts/ProtectedLayout.jsx
@@ -1,13 +1,27 @@
 import { Outlet, Navigate } from "react-router-dom"
 import useAuth from "../hooks/useAuth";
+import styles from '../styles/ProtectedLayout.module.css';
+import logo from '../assets/img/logo.png';
 
 const ProtectedLayout = () => {
     const { auth, loading } = useAuth();
 
     // TODO: Add loading spinner
-    if(loading) return <div>Loading...</div>;
-    return (
+    if(loading) return <div className={styles.loadercontainer}>
         <div>
+        <img className={styles["logo"]} src={logo} alt="Sparkaxe-logo" />
+        </div>
+       <div className={styles.row}>
+        <div className={styles.loader}>
+    <span className={styles.hour}></span>
+    <span className={styles.min}></span>
+    <span className={styles.circel}></span>
+  </div>
+  <h2 className={styles.Loading}> Sparkaxe</h2>
+  </div>
+  </div>;
+    return (
+        <div  className={styles.loaded}>
             {auth.id ? 
             (
                 <Outlet />
