235590a63a8f22a37c318a8489350e8b4b45cc0a
Diego Lopez-Araiza Soto <140852928+BlackaxolotlORG@users.noreply.github.com>
2024-05-10 22:37:32 -0600

    perfil tienda

--- frontend/src/components/Modals/ModalProfile.jsx
+++ frontend/src/components/Modals/ModalProfile.jsx
@@ -17,7 +17,58 @@ const ModalProfile = () => {
                 >
                     <i className="fa-solid fa-times"></i>
                 </button>
-                
+                <h2>Configuracion de perfil</h2>
+                <p>Nombre</p>
+                <p>Correo</p>
+                <p>Numero telefonico</p>
+                <p>Contraseña</p>
+                <p>Cambiar contraseña</p>
+                <p>Avatar</p>
+                <p>Websites</p>
+                <form 
+            className={styles.body}
+        >
+            <div className={styles.field}>
+                    <label htmlFor="">Tu nombre</label>
+                    <input 
+                        type="" 
+                        id="" 
+                        name="" 
+                        placeholder={`Nombre completo`}
+            
+                    />
+                </div>
+                <div className={styles.field}>
+                    <label htmlFor="">Correo</label>
+                    <input 
+                        type="" 
+                        id="" 
+                        name="" 
+                        placeholder={`ejemplo@correo.com`}
+            
+                    />
+                </div>
+                <div className={styles.field}>
+                    <label htmlFor="">Nombre del sitio web</label>
+                    <input 
+                        type="" 
+                        id="" 
+                        name="" 
+                        placeholder={`dominio.com`}
+            
+                    />
+                </div>
+                <div className={styles.field}>
+                    <label htmlFor="">Describe el problema</label>
+                    <textarea 
+                        type="" 
+                        id="" 
+                        name="" 
+                        placeholder={`Describe el problema`}
+            
+                    />
+                </div>
+                </form>
             </div>
         </div>
     )
