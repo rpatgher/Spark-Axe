5d9d0c605f9736c96481c7d41ece0dce3ebe86e8
Diego Lopez-Araiza Soto <140852928+BlackaxolotlORG@users.noreply.github.com>
2024-08-30 14:43:23 -0600

    push componentes

---
+++ frontend/src/components/LandingComponents/EnseñarFunctions/EnseñarFunctions.jsx
@@ -0,0 +1,12 @@
+const EnseñarFunctions = () => {
+    return (
+        <>
+            <div>
+                <h2>Enseñar Functions</h2>
+                <p>Aqui pone la rat el codigo</p>
+            </div>
+        </>
+    )
+}
+
+export default EnseñarFunctions;
