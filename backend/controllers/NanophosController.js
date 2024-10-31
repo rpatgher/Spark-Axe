c5e6efd6e0f0fa07175ba5764b41a3478b603c83
Remy Patgher <a01784177@tec.mx>
2024-10-30 20:52:25 -0600

    .

--- backend/controllers/NanophosController.js
+++ backend/controllers/NanophosController.js
@@ -1,120 +1,156 @@
-import { Op } from 'sequelize';
+import { Op, Sequelize } from "sequelize";
 
 // ************* Helpers *************
-import generateJWT from '../helpers/generateJWT.js';
-import generateToken from '../helpers/generateToken.js';
-import sendEmail from '../helpers/sendEmail.js';
+import generateJWT from "../helpers/generateJWT.js";
+import generateToken from "../helpers/generateToken.js";
+import sendEmail from "../helpers/sendEmail.js";
 
 // ************* Models *************
-import { Customer, Element, Category, Subcategory, Advertisement, PoS, Section } from '../models/index.js';
-
+import {
+    OrderElement,
+    Element,
+    Category,
+    Subcategory,
+    Advertisement,
+    PoS,
+    Section,
+} from "../models/index.js";
 
 const getInfo = async (req, res) => {
     const { website } = req;
-    if(!website){
-        const error = new Error('Website not found');
+    if (!website) {
+        const error = new Error("Website not found");
         return res.status(404).json({ msg: error.message });
     }
     const elements = await Element.findAll({
         where: {
             website_id: website.id,
-            published: true
+            published: true,
         },
         include: [
             {
                 model: Subcategory,
-                attributes: { exclude: ['category_id', 'createdAt', 'updatedAt', 'index', 'element_subcategory'] },
+                attributes: {
+                    exclude: [
+                        "category_id",
+                        "createdAt",
+                        "updatedAt",
+                        "index",
+                        "element_subcategory",
+                    ],
+                },
                 include: [
                     {
                         model: Category,
-                        attributes: { exclude: ['website_id', 'createdAt', 'updatedAt'] },
-                        order: [
-                            ['index', 'ASC']
-                        ]
-                    }
-                ]
-            }
+                        attributes: {
+                            exclude: ["website_id", "createdAt", "updatedAt"],
+                        },
+                        order: [["index", "ASC"]],
+                    },
+                ],
+            },
         ],
-        attributes: { exclude: ['website_id', 'createdAt', 'updatedAt'] },
-        order: [
-            ['index', 'ASC']
-        ]
+        attributes: { exclude: ["website_id", "createdAt", "updatedAt"] },
+        order: [["index", "ASC"]],
     });
     const categories = await Category.findAll({
         where: {
-            website_id: website.id
+            website_id: website.id,
         },
         include: [
             {
                 model: Subcategory,
-                attributes: { exclude: ['category_id', 'createdAt', 'updatedAt'] },
-                order: [
-                    ['index', 'ASC']
-                ]
-            }
+                attributes: {
+                    exclude: ["category_id", "createdAt", "updatedAt"],
+                },
+                order: [["index", "ASC"]],
+            },
         ],
-        attributes: { exclude: ['website_id','createdAt', 'updatedAt'] },
-        order: [
-            ['index', 'ASC']
-        ]
+        attributes: { exclude: ["website_id", "createdAt", "updatedAt"] },
+        order: [["index", "ASC"]],
     });
     const advertisements = await Advertisement.findAll({
         include: [
             {
                 model: Section,
                 where: {
-                    website_id: website.id
+                    website_id: website.id,
                 },
-                attributes: { exclude: ['website_id', 'createdAt', 'updatedAt'] }
-            }
+                attributes: {
+                    exclude: ["website_id", "createdAt", "updatedAt"],
+                },
+            },
         ],
-        attributes: { exclude: ['section_id', 'createdAt', 'updatedAt'] }
+        attributes: { exclude: ["section_id", "createdAt", "updatedAt"] },
     });
     const pos = await PoS.findAll({
         where: {
-            website_id: website.id
-        }
+            website_id: website.id,
+        },
+    });
+    const topProducts = await OrderElement.findAll({
+        attributes: [
+            "elementId",
+            [Sequelize.fn("SUM", Sequelize.col("quantity")), "totalQuantity"],
+        ],
+        group: ["elementId"],
+        order: [[Sequelize.literal("totalQuantity"), "DESC"]],
+        limit: 5,
+        include: [
+            {
+                model: Element,
+                attributes: [
+                    "id",
+                    "name",
+                    "description",
+                    "price",
+                    "color",
+                    "image",
+                ],
+                where: {
+                    website_id: website.id,
+                },
+            },
+        ],
     });
     res.json({
         elements,
         categories,
         advertisements,
-        pos
+        pos,
+        topProducts
     });
-}
+};
 
 const getOrders = async (req, res) => {
     const { customer } = req;
     const { website } = req;
-    if(customer.website_id !== website.id){
-        return res.status(401).json({ msg: 'User not allowed' });
+    if (customer.website_id !== website.id) {
+        return res.status(401).json({ msg: "User not allowed" });
     }
     try {
         const orders = await customer.getOrders({
             where: {
                 customer_id: customer.id,
                 status: {
-                    [Op.not]: 'C'
-                }
+                    [Op.not]: "C",
+                },
             },
             include: [
                 {
                     model: Element,
-                    attributes: ['id', 'name', 'price', 'image'],
+                    attributes: ["id", "name", "price", "image"],
                     through: {
-                        attributes: ['quantity']
-                    }
-                }
-            ]
+                        attributes: ["quantity"],
+                    },
+                },
+            ],
         });
         return res.status(200).json(orders);
     } catch (error) {
         console.log(error);
-        return res.status(400).json({ msg: 'An error ocurred' });
+        return res.status(400).json({ msg: "An error ocurred" });
     }
-}
+};
 
-export {
-    getOrders,
-    getInfo
-}
+export { getOrders, getInfo };
