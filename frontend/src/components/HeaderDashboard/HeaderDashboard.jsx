9d0c0a58ef92a38018844523d8d5f4f4754a31d8
Diego Lopez-Araiza Soto <140852928+BlackaxolotlORG@users.noreply.github.com>
2024-11-14 09:52:34 -0600

    Revert "share true info in whatsapp"
    This reverts commit 3bd24a791e91f8a0ad2b0ed85cfaeea47e79304d.

--- frontend/src/components/HeaderDashboard/HeaderDashboard.jsx
+++ frontend/src/components/HeaderDashboard/HeaderDashboard.jsx
@@ -4,8 +4,6 @@ import { Link } from 'react-router-dom';
 // **************** Hooks ****************
 import useApp from '../../hooks/useApp';
 import useAuth from '../../hooks/useAuth';
-import { useNavigate } from "react-router-dom";
-
 
 // **************** Styles ****************
 import styles from './HeaderDashboard.module.css';
@@ -14,13 +12,6 @@ const HeaderDashboard = () => {
     const { theme, handleToggleTheme } = useApp();
     const { logoutAuth, auth, setProfileModal } = useAuth();
     const { name, lastname, websites, role } = auth;
-    const navigate = useNavigate();
-
-    const handleLogoutAndRedirect = () => {
-    logoutAuth(); // Perform the logout function
-    setProfileModal(false); // Close the profile modal, if necessary
-    navigate("/"); // Redirect to the root ("/")
-  };
 
     return (
         <header className={styles.header}>
@@ -68,7 +59,7 @@ const HeaderDashboard = () => {
                             <i className="fa-solid fa-user-gear"></i>Perfil 
                         </button>
                         <button
-                            onClick={handleLogoutAndRedirect}
+                            onClick={logoutAuth}
                         ><i className="fa-solid fa-right-from-bracket"></i>Cerrar Sesión</button>
                     </div>
                     </div>
